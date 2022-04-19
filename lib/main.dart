4f7589a1ed037efb75592638dc718cf32da9338a
Ardit Daku <arditdaku1@gmail.com>
2022-04-19 15:29:47 +0200

    Created Text Field & Working on Chat Messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Friendly Chat',
-    home: Scaffold(
+  runApp(const FriendlyChatApp());
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Friendly Chat', home: ChatScreen());
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('Friendly Chat'),
+        title: const Text('FriendlyChat'),
       ),
-    ),
-  ));
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        children: [
+          Flexible(
+            child: TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmit,
+              decoration:
+                  const InputDecoration.collapsed(hintText: 'Send a message'),
+            ),
+          ),
+          IconTheme(
+            data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () => _handleSubmit(_textController.text),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleSubmit(String text) {
+    _textController.clear();
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  const ChatMessage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            // child: ,
+          ),
+        ],
+      ),
+    );
+  }
 }
